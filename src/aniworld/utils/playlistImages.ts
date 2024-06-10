36509763b357d8edbc5118506c6c79363da0744e
Shizu567 <124355315+Shizu567@users.noreply.github.com>
2024-06-10 20:00:57 +0530

    Update playlistImages.ts

--- src/aniworld/utils/playlistImages.ts
+++ src/aniworld/utils/playlistImages.ts
@@ -1,21 +1,22 @@
 import * as cheerio from "cheerio";
+import axios from 'axios';
 
 export const getPlaylistImages = async (id: string, posterSize: 150 | 200 | 220 = 220) => {
-  const url = `https://aniworld.to/anime/stream/${id}`;
-  const response = await request.get(url);
-  const $ = cheerio.load(response.text());
+  const url = `https://asianc.to/drama/stream/${id}`;
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
 
-  const posterImage = $(".seriesCoverBox > img").attr("data-src");
+  const posterImage = $(".poster-image-class > img").attr("data-src"); // Update this selector
 
-  const bannerImage = $(".backdrop")
+  const bannerImage = $(".banner-image-class")
     .attr("style")
     ?.match(/url\(([^)]+)\)/)?.[1];
 
   return {
     posterImage: posterImage
-      ? convertPosterSize(`https://aniworld.to${posterImage}`, posterSize)
+      ? convertPosterSize(`https://asianc.to${posterImage}`, posterSize)
       : undefined,
-    bannerImage: bannerImage ? `https://aniworld.to${bannerImage}` : undefined,
+    bannerImage: bannerImage ? `https://asianc.to${bannerImage}` : undefined,
   };
 };
 
