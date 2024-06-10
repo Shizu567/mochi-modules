4c728ef5ebcc80ea3b297b103a67c477e24ec528
Shizu567 <124355315+Shizu567@users.noreply.github.com>
2024-06-10 20:04:10 +0530

    Update index.ts

--- src/aniworld/index.ts
+++ src/aniworld/index.ts
@@ -23,58 +23,52 @@ import * as cheerio from "cheerio";
 import { scrapeAvailableLanguages, scrapeGenres, scrapeGroups, scrapeSynopsis } from "./scraper";
 import { ExtractorId, extract, extractors } from "$shared/extractors";
 import { getM3u8Qualities } from "$shared/utils";
+import axios from 'axios';
 
-// very ugly hack to change BASE_URL
-// this is needed bc mochi js implementation cannot read class properties
-// pls fix erik :-(
-let BASE_URL = "https://aniworld.to";
+let BASE_URL = "https://asianc.to";
 
-export default class AniWorld extends SourceModule implements VideoContent {
+export default class Dramacool extends SourceModule implements VideoContent {
   static LANGUAGES = new Map(
     Object.entries({
-      german: "GerDub",
-      "japanese-german": "GerSub",
+      korean: "KorSub",
+      "chinese-english": "EngSub",
       "japanese-english": "EngSub",
     })
   );
 
   metadata = {
-    id: "aniworld",
-    name: "AniWorld",
-    description: "Module to watch anime from AniWorld",
+    id: "dramacool",
+    name: "Dramacool",
+    description: "Module to watch dramas from Dramacool",
     icon: `${BASE_URL}/favicon.ico`,
-    version: "1.1.2",
+    version: "1.0.0",
   };
 
   constructor(baseUrl?: string) {
     super();
-    BASE_URL = baseUrl || "https://aniworld.to";
-  }
-
-  async searchFilters(): Promise<SearchFilter[]> {
-    return [];
+    BASE_URL = baseUrl || "https://asianc.to";
   }
 
   async search(query: SearchQuery): Promise<Paging<Playlist>> {
-    const response = await request.post(`${BASE_URL}/ajax/search`, {
-      body: `keyword=${query.query}`,
+    const response = await axios.post(`${BASE_URL}/ajax/search`, {
+      params: { keyword: query.query },
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
     });
-    const data = response.json() as { title: string; description: string; link: string }[];
+    const data = response.data as { title: string; description: string; link: string }[];
 
     const ids: string[] = [];
 
     const playlists: Promise<Playlist>[] = data
-      .filter(({ link }) => link.startsWith("/anime/stream/")) // animes only
-      .filter(({ link }) => link.split("/").length === 4) // no specific season or episode results
+      .filter(({ link }) => link.startsWith("/drama/stream/"))
+      .filter(({ link }) => link.split("/").length === 4)
       .filter(({ link }) => {
         const id = link.split("/")[3];
         if (ids.includes(id)) return false;
         ids.push(id);
         return true;
-      }) // remove duplicate results
+      })
       .map(async (item) => {
         const { id } = getUrlInfo(item.link);
         const { posterImage, bannerImage } = await getPlaylistImages(id);
@@ -92,17 +86,15 @@ export default class AniWorld extends SourceModule implements VideoContent {
 
     return {
       id: "",
-      items: await Promise.all(playlists), // thx @ElNixu for parallel loading of images
+      items: await Promise.all(playlists),
     };
   }
 
   async discoverListings(): Promise<DiscoverListing[]> {
-    console.log(BASE_URL);
+    const response = await axios.get(`${BASE_URL}`);
+    const $ = cheerio.load(response.data);
 
-    const response = await request.get(`${BASE_URL}`);
-    const $ = cheerio.load(response.text());
-
-    const carousels = $(".carousel:not(.animeNews)")
+    const carousels = $(".carousel:not(.dramaNews)")
       .toArray()
       .map((carousel) => {
         return {
@@ -137,8 +129,8 @@ export default class AniWorld extends SourceModule implements VideoContent {
   }
 
   async playlistDetails(id: string): Promise<PlaylistDetails> {
-    const response = await request.get(`${BASE_URL}/anime/stream/${id}`);
-    const $ = cheerio.load(response.text());
+    const response = await axios.get(`${BASE_URL}/drama/stream/${id}`);
+    const $ = cheerio.load(response.data);
 
     let yearReleased;
     try {
@@ -165,8 +157,8 @@ export default class AniWorld extends SourceModule implements VideoContent {
   ): Promise<PlaylistItemsResponse> {
     const groupId = options?.groupId || "";
 
-    const response = await request.get(`${BASE_URL}/anime/stream/${playlistId}/${groupId}`);
-    const $ = cheerio.load(response.text());
+    const response = await axios.get(`${BASE_URL}/drama/stream/${playlistId}/${groupId}`);
+    const $ = cheerio.load(response.data);
 
     return scrapeGroups($);
   }
@@ -177,10 +169,10 @@ export default class AniWorld extends SourceModule implements VideoContent {
     const { playlistId, episodeId: _episodeId } = req;
     const [groupId, episodeId, variantId] = _episodeId.split("/");
 
-    const response = await request.get(
-      `${BASE_URL}/anime/stream/${playlistId}/${groupId}/${episodeId}`
+    const response = await axios.get(
+      `${BASE_URL}/drama/stream/${playlistId}/${groupId}/${episodeId}`
     );
-    const $ = cheerio.load(response.text());
+    const $ = cheerio.load(response.data);
 
     const availableLanguages = scrapeAvailableLanguages($);
     const langKey =
@@ -189,18 +181,18 @@ export default class AniWorld extends SourceModule implements VideoContent {
 
     const servers = $(".hosterSiteVideo > ul.row > li")
       .toArray()
-      .filter((li) => $(li).attr("data-lang-key") == langKey) // filter by language
+      .filter((li) => $(li).attr("data-lang-key") == langKey)
       .map((li) => {
         const id = $(li).attr("data-link-target")!.split("/").at(-1)!;
         const displayName = $(li).find("h4").text();
         return { id: `${displayName.toLowerCase()}/${id}`, displayName };
       })
-      .filter(({ displayName }) => Object.keys(extractors).includes(displayName.toLowerCase())); // filter for supported servers
+      .filter(({ displayName }) => Object.keys(extractors).includes(displayName.toLowerCase()));
 
     return [
       {
-        id: "aniworld",
-        displayName: "AniWorld",
+        id: "dramacool",
+        displayName: "Dramacool",
         description: undefined,
         servers,
       },
